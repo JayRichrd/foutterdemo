6384627fd035f6089a30656e0bad103c31d3afb2
cainjiang <cainjiang@tencent.com>
2019-03-13 11:27:56 +0800

    feat(text): 添加图标组件

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,7 @@ class MyApp extends StatelessWidget {
     final appName = '自定义主题';
     return MaterialApp(
       title: '容器组件示例',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('容器组件示例'),
-//        ),
-//        body: Center(
-//          child: Container(
-//            width: 200,
-//            height: 200,
-//            decoration: BoxDecoration(
-//              color: Colors.white,
-//              border: Border.all(
-//                color: Colors.grey,
-//                width: 8.0,
-//              ),
-//              borderRadius: const BorderRadius.all(const Radius.circular(10.0)),
-//            ),
-//            child: Text(
-//              'Text',
-//              textAlign: TextAlign.center,
-//              style: TextStyle(fontSize: 30.0),
-//            ),
-//          ),
-//        ),
-//      ),
-      home: ImageDemo(),
+      home: TextContainerDemo(),
     );
   }
 
@@ -69,6 +45,54 @@ class ImageDemo extends StatelessWidget {
       child: Image.network(
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552456810628&di=29016e3770f818c44c7c82084455d577&imgtype=0&src=http%3A%2F%2Fpic5.nipic.com%2F20091223%2F3279936_153719006353_2.jpg',
         fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+}
+
+class TextContainerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('文本组件'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            '红色+黑色删除线+25号',
+            style: TextStyle(
+              color: const Color(0xffff0000),
+              decoration: TextDecoration.lineThrough,
+              decorationColor: const Color(0xff000000),
+              fontSize: 25.0,
+            ),
+          ),
+          Text(
+            '橙色+黑色删除线+25号',
+            style: TextStyle(
+              color: const Color(0xffff9900),
+              decoration: TextDecoration.underline,
+              fontSize: 25.0,
+            ),
+          ),
+          Text(
+            '虚线上划线+25号+倾斜',
+            style: TextStyle(
+                decoration: TextDecoration.overline,
+                decorationStyle: TextDecorationStyle.dashed,
+                fontSize: 25.0,
+                fontStyle: FontStyle.italic),
+          ),
+          Text(
+            '加粗+25号',
+            style: TextStyle(
+                fontSize: 25.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 6.0),
+          ),
+        ],
       ),
     );
   }
