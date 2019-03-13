6a2fb9eb60d6474240c5383effae30d173e07543
cainjiang <cainjiang@tencent.com>
2019-03-13 15:48:54 +0800

    feat(list): 列表组件

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,36 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MyApp(items: List<String>.generate(500, (i) => "Item $i")));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final appName = '自定义主题';
+//    return MaterialApp(
+//      title: 'Flutter Demo',
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('长列表'),
+//        ),
+//        body: ListView.builder(
+//          itemCount: items.length,
+//          itemBuilder: (context, index) {
+//            return ListTile(
+//              leading: Icon(Icons.phone),
+//              title: Text('${items[index]}'),
+//            );
+//          },
+//        ),
+//      ),
+//    );
     return MaterialApp(
-      title: '容器组件示例',
-      home: LayoutDemo(),
+      home: GridDemoLayout(),
     );
   }
 
@@ -35,6 +56,118 @@ class MyApp extends StatelessWidget {
     } catch (e) {
       print("请求失败：$e");
     } finally {}
+  }
+}
+
+class GridDemoLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('网格组件'),
+        ),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20.0),
+          crossAxisSpacing: 30.0,
+          crossAxisCount: 4,
+          children: <Widget>[
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+            const Text('第一行第一列'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HorizontalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '水平列表',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('水平列表'),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.lightBlue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.amber,
+              ),
+              Container(
+                width: 160.0,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '水平',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+                    ),
+                    Text(
+                      '列表',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class VerticalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '容器组件示例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('基础列表'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text('Alarm'),
+            ),
+            ListTile(
+              leading: Icon(Icons.airplay),
+              title: Text('AirPlay'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
